ed7f0d673f76e2f8a02ed713e80adad808661caa
tylts <59815381+tylts@users.noreply.github.com>
2023-08-19 20:59:14 -0500

    Moved the event listener functions into their own cozy function homes.

--- script.js
+++ script.js
@@ -16,124 +16,13 @@ const equalsBtn = document.querySelector("#equals");
 const decimalBtn = document.querySelector(".decimal-button");
 const percentBtn = document.querySelector(".percent-button");
 
-numberBtns.forEach((e) => {
-  e.addEventListener("click", () => {
-    if (displayValue.value === "0") displayValue.value = "";
-    if (clearDisplayNextInput === true) {
-      displayValue.value = "";
-      clearDisplayNextInput = false;
-    }
-    if (firstNum || firstNum === 0) {
-      displayValue.value += e.textContent;
-      secondNum = Number(displayValue.value);
-      return;
-    }
-    displayValue.value += e.textContent;
-  });
-});
-
-decimalBtn.addEventListener("click", () => {
-  if (displayValue.value.includes(".") && clearDisplayNextInput === false) {
-    return;
-  }
-
-  if (clearDisplayNextInput === true) {
-    displayValue.value = "0.";
-    clearDisplayNextInput = false;
-    return;
-  }
-  displayValue.value += decimalBtn.textContent;
-});
-
-operatorBtns.forEach((e) => {
-  e.addEventListener("click", () => {
-    firstNum = Number(displayValue.value);
-    secondNumIsNext = true;
-    if (operationChain === true) {
-      secondNum = Number(displayValue.value);
-      displayValue.value = Number(
-        operate(firstNum, secondNum, operator).toFixed(7)
-      );
-      operator = e.textContent;
-    }
-
-    if (operator != null) {
-      operator = e.textContent;
-      firstNum = Number(displayValue.value);
-      if (displayValue.value === "divide by 0? lol") firstNum = 0;
-      operationChain = true;
-      clearDisplayNextInput = true;
-      equalsChain = false;
-      return;
-    }
-
-    operator = e.textContent;
-    clearDisplayNextInput = true;
-    operationChain = true;
-    equalsChain = false;
-  });
-});
-
-percentBtn.addEventListener("click", () => {
-  if (firstNum === 100) {
-    secondNum = Number(((firstNum / 100) * secondNum).toFixed(7));
-  }
-  if (clearDisplayNextInput === false && !secondNumIsNext) {
-    firstNum = Number((displayValue.value / 100).toFixed(7));
-    displayValue.value = firstNum;
-    clearDisplayNextInput = true;
-  }
-
-  if (secondNumIsNext) {
-    if (operator === "+" || operator === "-") {
-      console.log((secondNum / 100) * firstNum);
-      displayValue.value = Number(((secondNum / 100) * firstNum).toFixed(7));
-      secondNum = Number(displayValue.value);
-    }
-
-    if (operator === "×" || operator === "÷") {
-      console.log((secondNum / 100) * firstNum);
-      displayValue.value = Number((secondNum / 100).toFixed(7));
-      secondNum = Number(displayValue.value);
-    }
-  }
-});
-
+numberBtns.forEach((e) => e.addEventListener("click", numberFunction(e)));
+decimalBtn.addEventListener("click", decimalFunction);
+operatorBtns.forEach((e) => e.addEventListener("click", operatorFunction(e)));
+percentBtn.addEventListener("click", percentFunction);
 clearBtn.addEventListener("click", clearCalc);
-
-signToggle.addEventListener("click", () => {
-  if (displayValue.value === "0") return;
-
-  Number(displayValue.value) > 0
-    ? (displayValue.value = `-${displayValue.value}`)
-    : (displayValue.value = displayValue.value.slice(1));
-});
-
-equalsBtn.addEventListener("click", () => {
-  if (isNaN(firstNum) || isNaN(secondNum) || !operator) {
-    console.log(firstNum, operator, secondNum);
-    clearCalc();
-    displayValue.value = "You messed up.";
-    console.log("triggered in equals");
-    return;
-  }
-  if (equalsChain) {
-    let constantNum = secondNum;
-    firstNum = Number(displayValue.value);
-    displayValue.value = Number(
-      operate(firstNum, constantNum, operator).toFixed(7)
-    );
-    return;
-  }
-
-  displayValue.value = Number(
-    operate(firstNum, secondNum, operator).toFixed(7)
-  );
-  operate(firstNum, secondNum, operator);
-  clearDisplayNextInput = true;
-  operationChain = false;
-  equalsChain = true;
-});
+signToggle.addEventListener("click", signToggleFunction);
+equalsBtn.addEventListener("click", equalsFunction);
 
 function addition(a, b) {
   return a + b;
@@ -184,4 +73,121 @@ function clearCalc() {
   clearDisplayNextInput = true;
   operationChain = false;
   secondNumIsNext = false;
+}
+
+function numberFunction(e) {
+  e.addEventListener("click", () => {
+    if (displayValue.value === "0") displayValue.value = "";
+    if (clearDisplayNextInput === true) {
+      displayValue.value = "";
+      clearDisplayNextInput = false;
+    }
+    if (firstNum || firstNum === 0) {
+      displayValue.value += e.textContent;
+      secondNum = Number(displayValue.value);
+      return;
+    }
+    displayValue.value += e.textContent;
+  });
+}
+
+function decimalFunction() {
+  if (displayValue.value.includes(".") && clearDisplayNextInput === false) {
+    return;
+  }
+
+  if (clearDisplayNextInput === true) {
+    displayValue.value = "0.";
+    clearDisplayNextInput = false;
+    return;
+  }
+  displayValue.value += decimalBtn.textContent;
+}
+
+function operatorFunction(e) {
+  e.addEventListener("click", () => {
+    firstNum = Number(displayValue.value);
+    secondNumIsNext = true;
+    if (operationChain === true) {
+      secondNum = Number(displayValue.value);
+      displayValue.value = Number(
+        operate(firstNum, secondNum, operator).toFixed(7)
+      );
+      operator = e.textContent;
+    }
+
+    if (operator != null) {
+      operator = e.textContent;
+      firstNum = Number(displayValue.value);
+      if (displayValue.value === "divide by 0? lol") firstNum = 0;
+      operationChain = true;
+      clearDisplayNextInput = true;
+      equalsChain = false;
+      return;
+    }
+
+    operator = e.textContent;
+    clearDisplayNextInput = true;
+    operationChain = true;
+    equalsChain = false;
+  });
+}
+
+function percentFunction() {
+  if (firstNum === 100) {
+    secondNum = Number(((firstNum / 100) * secondNum).toFixed(7));
+  }
+  if (clearDisplayNextInput === false && !secondNumIsNext) {
+    firstNum = Number((displayValue.value / 100).toFixed(7));
+    displayValue.value = firstNum;
+    clearDisplayNextInput = true;
+  }
+
+  if (secondNumIsNext) {
+    if (operator === "+" || operator === "-") {
+      console.log((secondNum / 100) * firstNum);
+      displayValue.value = Number(((secondNum / 100) * firstNum).toFixed(7));
+      secondNum = Number(displayValue.value);
+    }
+
+    if (operator === "×" || operator === "÷") {
+      console.log((secondNum / 100) * firstNum);
+      displayValue.value = Number((secondNum / 100).toFixed(7));
+      secondNum = Number(displayValue.value);
+    }
+  }
+}
+
+function signToggleFunction() {
+  if (displayValue.value === "0") return;
+
+  Number(displayValue.value) > 0
+    ? (displayValue.value = `-${displayValue.value}`)
+    : (displayValue.value = displayValue.value.slice(1));
+}
+
+function equalsFunction() {
+  if (isNaN(firstNum) || isNaN(secondNum) || !operator) {
+    console.log(firstNum, operator, secondNum);
+    clearCalc();
+    displayValue.value = "You messed up.";
+    console.log("triggered in equals");
+    return;
+  }
+  if (equalsChain) {
+    let constantNum = secondNum;
+    firstNum = Number(displayValue.value);
+    displayValue.value = Number(
+      operate(firstNum, constantNum, operator).toFixed(7)
+    );
+    return;
+  }
+
+  displayValue.value = Number(
+    operate(firstNum, secondNum, operator).toFixed(7)
+  );
+  operate(firstNum, secondNum, operator);
+  clearDisplayNextInput = true;
+  operationChain = false;
+  equalsChain = true;
 }
